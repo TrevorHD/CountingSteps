f6711df5452301153b7fb33b791d1436b0a38ffc
TrevorHD <44789861+TrevorHD@users.noreply.github.com>
2021-07-19 12:24:03 -0400

    File upload 2
    Uploaded the beginning of the data analysis script.

---
+++ CountingSteps.R
@@ -0,0 +1,20 @@
+##### Load libraries and initialise data ------------------------------------------------------------------
+
+# Load libraries
+library(tidyverse)
+
+# Load in step data
+data <- read.csv("StepData.csv")
+
+
+
+
+
+##### Plot distribution of daily step counts --------------------------------------------------------------
+
+
+
+
+
+##### Plot moving average and daily counts ----------------------------------------------------------------
+
