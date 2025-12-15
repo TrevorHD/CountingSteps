84e73d659a3764fedec93b43257568e9aee2d8c7
TrevorHD <44789861+TrevorHD@users.noreply.github.com>
2025-12-15 14:51:30 -0800

    Content update 4
    Made significant changes to how data is presented, breaking visualisations and summary stats down by year. Updated data to include placeholder dates out until EoY 2029.

--- Scripts/CountingSteps.R
+++ Scripts/CountingSteps.R
@@ -16,136 +16,192 @@ data <- read.csv("Data/StepData.csv")
 names(data)[1] <- "Year"
 data <- mutate(data, Date = as.Date(paste(Month, Day, Year, sep = "/"), "%m/%d/%Y"))
 
-# Set ceiling and round down any higher observations (only 2 so far)
-data$Steps[data$Steps > 40000] <- 40000
 
 
 
 
-
-##### Plot distribution of daily step counts --------------------------------------------------------------
-
-# Function to generate histogram
-hPlot <- function(){
-  par(lwd = 0.5)
-  histColours <-  c(colorRampPalette(c("red4", "red"), 0.5)(8), 
-                    colorRampPalette(c("green", "green4"))(8), rep("green4", 24))
-  h <- hist(data$Steps, breaks = seq(0, 40000, by = 1000), plot = FALSE)
-  h$density <- h$counts/sum(h$counts)*100
-  plot(h, freq = FALSE, xlab = "", xaxt = "n",
-       ylab = "", yaxt = "n", col = histColours, main = "", ylim = c(0, 30))
-  axis(side = 1, at = seq(0, 40000, by = 5000), labels = c("0", "", "10k", "", "20k", "", "30k", "", "40k"))
-  axis(side = 2, at = seq(0, 30, by = 5), labels = c("0%", "", "10%", "", "20%", "", "30%"),
-       mgp = c(0, 0.17, 0))
-  abline(v = 8000, col = "blue", lty = 3, lwd = 0.5)
-  box(lwd = 1)}
-
-
-
-
-
-##### Plot moving average and daily counts ----------------------------------------------------------------
+##### Set up plotting functions ---------------------------------------------------------------------------
 
 # Function to generate time series plot
-tPlot <- function(){
-  plot(data$Date, data$Steps, type = "l", ylim = c(0, 40000), xlab = "", xaxt = "n",
-       ylab = "", yaxt = "n", lwd = 0.5)
-  axis.Date(1, at = seq(min(data$Date), max(data$Date), by = "3 mon"), format = "%m/%y", cex.axis = 0.4)
-  axis.Date(1, at = seq(min(data$Date), max(data$Date), by = "1 mon"), labels = FALSE, tcl = -0.11)
-  axis(side = 2, at = seq(0, 40000, by = 5000), labels = c("0", "", "10k", "", "20k", "", "30k", "", "40k"),
-       mgp = c(0, 0.17, 0))
-  lines(data$Date, rollmean(data$Steps, 7, fill = list(NA, NULL, NA)), col = "red", lwd = 1)
-  abline(h = 8000, col = "blue", lty = 3, lwd = 0.5)}
+tPlot <- function(data, bottom = FALSE){
+  plot(data$Date, data$Steps, type = "l", ylim = c(0, 30000),
+       xlab = "", xaxt = "n", ylab = "", yaxt = "n", lwd = 0.50)
+  axis(side = 2, at = seq(0, 30000, by = 5000),
+       labels = c("0", "", "10k", "", "20k", "", "30k"),
+       mgp = c(0.00, 0.17, 0.00))
+  lines(data$Date, rollmean(data$Steps, 7, fill = list(NA, NULL, NA)),
+        col = "red", lwd = 1)
+  if(!is.na(data$Steps[1])){
+    abline(h = 8000, col = "blue", lty = 3, lwd = 0.50)}
+  if(bottom == TRUE){
+    axis.Date(1, at = seq(min(data$Date), max(data$Date), by = "1 mon"),
+              labels = month.abb, tcl = -0.20)}
+  if(bottom == FALSE){
+    axis.Date(1, at = seq(min(data$Date), max(data$Date), by = "1 mon"),
+              labels = FALSE, tcl = -0.12)}}
+
+# Function to generate histogram
+hPlot <- function(data, bottom = FALSE){
+  par(lwd = 0.50)
+  data$Steps[data$Steps > 30000] <- 30000
+  histColours <-  c(colorRampPalette(c("red4", "red"), 0.50)(8), 
+                    colorRampPalette(c("green", "green4"))(8), rep("green4", 24))
+  h <- hist(data$Steps, breaks = seq(0, 30000, by = 1000), plot = FALSE)
+  h$density <- h$counts/sum(h$counts)*100
+  plot(h, freq = FALSE, ylim = c(0, 42), main = "",
+       xlab = "", xaxt = "n",  ylab = "", yaxt = "n", col = histColours)
+  axis(side = 4, at = seq(0, 42, by = 7),
+       labels = c("0%", "", "14%", "", "28%", "", "42%"),
+       mgp = c(0.00, -0.15, 0.00))
+  box(lwd = 1.00)
+  if(!is.na(data$Steps[1])){
+    abline(v = 8000, col = "blue", lty = 3, lwd = 0.50)}
+  if(bottom == TRUE){
+    axis(side = 1, at = seq(0, 30000, by = 5000),
+         labels = c("0", "5k", "10k", "15k", "20k", "25k", "30k+"), tcl = -0.20)}
+  if(bottom == FALSE){
+    axis(side = 1, at = seq(0, 30000, by = 5000),
+         labels = FALSE, tcl = -0.12)}}
+
+# Function to assist in plotting YoY indicators
+dInd <- function(num, type){
+  if(type == "col"){
+    inds <- ifelse(num < 0, "red", ifelse(num > 0, "green", "black"))}
+  if(type == "pch"){
+    inds <- ifelse(num < 0, 25, ifelse(num > 0, 24, 22))}
+  if(type == "adj"){
+    inds <- ifelse(num < 0, 0.002, ifelse(num > 0, -0.002, 0))}
+  return(inds)}
 
 
 
 
 
-##### Get overall step count stats ------------------------------------------------------------------------
+##### Prepare data for plotting ---------------------------------------------------------------------------
 
-# Get longest goal streak
-goalStreak <- rle(data$Steps >= 8000)
-goalStreak <- max(goalStreak$lengths[goalStreak$values == TRUE])
+# Calculate key stats by year
+data %>% group_by(Year) %>% 
+  summarise(av = round(mean(Steps, na.rm = TRUE),0),
+            ma = max(Steps, na.rm = TRUE),
+            st = max(rle(Steps >= 8000)$lengths),
+            pt = sprintf("%0.2f%%", sum(Steps >= 8000, na.rm = TRUE)/sum(!is.na(Steps))*100)) -> data_y
+for(i in 1:nrow(data_y)){
+  if(is.nan(data_y$av[i])){
+    data_y[i, 2:ncol(data_y)] <- rep(NA, 4)}}
 
-# Calculate total steps, total distance, and average daily steps
-# Also calculate rate that goal is met; include longest goal streak
-stepStats <- c(sum(data$Steps),
-               round(sum(data$Steps)*0.427/1000, 0),
-               round(mean(data$Steps), 0),
-               round(length(data$Steps[data$Steps >= 8000])/nrow(data)*100, 2),
-               goalStreak)
+# Calculate YoY deltas for key stats
+data_d <- data_y
+data_d$pt <- as.numeric(gsub("%", "", data_d$pt))
+data_d <- tail(data_d, -1) - head(data_d, -1)
+data_d <- rbind(c(1, 0, 0, 0, 0), data_d)
+data_d$Year <- data_y$Year
 
-# Get total and mean step count, goal rate over last 30 days
-data30 <- data[(nrow(data)-30):nrow(data), ]
-step30Stats <- c(sum(data30$Steps),
-                 round(mean(data30$Steps), 0),
-                 round(length(data30$Steps[data30$Steps >= 8000])/nrow(data30)*100, 2))
-
-
-
+# Add helper columns for plotting YoY indicators
+data_d %>% mutate(avC = dInd(av, "col"), maC = dInd(ma, "col"),
+                  stC = dInd(st, "col"), ptC = dInd(pt, "col"),
+                  avP = dInd(av, "pch"), maP = dInd(ma, "pch"),
+                  stP = dInd(st, "pch"), ptP = dInd(pt, "pch"),
+                  avA = dInd(av, "adj"), maA = dInd(ma, "adj"),
+                  stA = dInd(st, "adj"), ptA = dInd(pt, "adj")) -> data_d
 
 
-##### Combine plots into a single canvas ------------------------------------------------------------------
+
+
+
+##### Plot data -------------------------------------------------------------------------------------------
+
+# Set years to plot; must be 5-year window
+pYears <- 2025:2029
+
+# Filter data to selected years
+data_py <- filter(data_y, Year %in% pYears)
+data_pd <- filter(data_d, Year %in% pYears)
 
 # Prepare graphics device
-tiff(filename = "StepData.tif", width = 3304, height = 2272, units = "px", res = 800, compression = "lzw")
+tiff(filename = "Figures/StepData_2025_2029.tif", width = 4800, height = 4000,
+     units = "px", res = 800, compression = "lzw")
 
 # Create blank page
 grid.newpage()
 plot.new()
 
 # Set grid layout and activate it
-gly <- grid.layout(800, 1200)
+gly <- grid.layout(1000, 1200)
 pushViewport(viewport(layout = gly))
 
-# Plot step count time series
-pushViewport(vp = viewport(layout.pos.row = 370:790, layout.pos.col = 1:1175))
-par(fig = gridFIG(), mar = c(0.8, 1, 0.4, 0.4), cex.axis = 0.42, tcl = -0.2, mgp = c(0, -0.2, 0))
-par(new = TRUE)
-tPlot()
-popViewport()
-
-# Plot step count histogram
-pushViewport(vp = viewport(layout.pos.row = 15:350, layout.pos.col = 1:700))
-par(fig = gridFIG(), mar = c(0.3, 1, 0.4, 0.4), cex.axis = 0.42, tcl = -0.2, mgp = c(0, -0.2, 0))
-par(new = TRUE)
-hPlot()
-popViewport()
-
-# Create figure labels
-grid.text(label = c("Daily Step Count", "(Distribution)", "Daily Step Count", "(Time Series)"),
-          x = c(0.549, 0.549, 0.061, 0.061), y = c(0.932, 0.915, 0.488, 0.471),
-          hjust = c(1, 1, 0, 0), gp = gpar(cex = 0.3))
-
-# Create legend (top)
+# Plot up to 5 years of step count time series, plus legend
+pCols <- seq(1, 783, length.out = length(pYears))
+for(i in 1:length(pYears)){
+  if(i < length(pYears)){
+    pushViewport(vp = viewport(layout.pos.row = (pCols[i]):(pCols[i + 1]), layout.pos.col = 1:590))
+    par(fig = gridFIG(), mar = c(0.30, 1.00, 0.30, 0.00), cex.axis = 0.40,
+        tcl = -0.20, mgp = c(0.00, -0.20, 0.00))
+    par(new = TRUE)
+    tPlot(filter(data, Year == pYears[i]))}
+  if(i == length(pYears)){
+    pushViewport(vp = viewport(layout.pos.row = 784:990, layout.pos.col = 1:590))
+    par(fig = gridFIG(), mar = c(0.60, 1.00, 0.30, 0.00), cex.axis = 0.40,
+        tcl = -0.20, mgp = c(0.00, -0.20, 0.00))
+    par(new = TRUE)
+    tPlot(filter(data, Year == pYears[i]), bottom = TRUE)}
+  popViewport()}
+grid.text(label = c("Daily Goal", "7-Day Avg."),
+          x = rep(0.057, 2), y = c(0.940, 0.928),
+          hjust = 0, gp = gpar(cex = 0.35))
+grid.segments(x0 = rep(0.040, 2), y0 = c(0.940, 0.928),
+              x1 = rep(0.051, 2), y1 = c(0.940, 0.928),
+              gp = gpar(col = c("blue", "red"), lty = c(3, 1), lwd = 0.50))
+  
+# Plot up to 5 years of step count histograms, plus legend
+for(i in 1:length(pYears)){
+  if(i < length(pYears)){
+    pushViewport(vp = viewport(layout.pos.row = (pCols[i]):(pCols[i + 1]), layout.pos.col = 591:865))
+    par(fig = gridFIG(), mar = c(0.30, 0.00, 0.30, 0.30), cex.axis = 0.40,
+        tcl = -0.20, mgp = c(0.00, -0.20, 0.00))
+    par(new = TRUE)
+    hPlot(filter(data, Year == pYears[i]))}
+  if(i == length(pYears)){
+    pushViewport(vp = viewport(layout.pos.row = 784:990, layout.pos.col = 591:865))
+    par(fig = gridFIG(), mar = c(0.60, 0.00, 0.30, 0.30), cex.axis = 0.40,
+        tcl = -0.20, mgp = c(0.00, -0.20, 0.00))
+    par(new = TRUE)
+    hPlot(filter(data, Year == pYears[i]), bottom = TRUE)}
+  popViewport()}
 grid.text(label = c("Above Goal", "Below Goal"),
-          x = rep(0.486, 2), y = c(0.882, 0.865),
-          hjust = 0, gp = gpar(cex = 0.3))
-grid.rect(x = rep(0.476, 2), y = c(0.882, 0.865),
-          width = rep(0.008, 2), height = rep(0.010, 2),
+          x = rep(0.688, 2), y = c(0.940, 0.928),
+          hjust = 1, gp = gpar(cex = 0.35))
+grid.rect(x = rep(0.700, 2), y = c(0.940, 0.928),
+          width = rep(0.008, 2), height = rep(0.007, 2),
           gp = gpar(col = c("green", "red"), fill = c("green", "red")))
 
-# Create legend (bottom)
-grid.text(label = c("Daily Count", "7-Day Average", "Daily Goal"),
-          x = rep(0.085, 3), y = c(0.440, 0.423, 0.408),
-          hjust = 0, gp = gpar(cex = 0.3))
-grid.segments(x0 = rep(0.061, 3), y0 = c(0.443, 0.426, 0.411),
-              x1 = rep(0.081, 3), y1 = c(0.443, 0.426, 0.411),
-              gp = gpar(col = c("black", "red", "blue"), lty = c(1, 1, 3), lwd = 0.5))
+# Add figure labels
+grid.text(label = c("Daily Step Count", "(Distribution)", "Daily Step Count", "(Time Series)"),
+          x = c(0.039, 0.039, 0.705, 0.705), y = c(0.976, 0.961, 0.976, 0.961),
+          hjust = c(0, 0, 1, 1), gp = gpar(cex = 0.40))
 
-# Summary stats
-grid.text(label = c(paste0(stepStats[2], " km"), "Total Distance",
-                    paste0(stepStats[1]), "Total Steps",
-                    paste0(stepStats[3]), "Average Daily Steps",
-                    paste0(sprintf(stepStats[4], fmt = "%#.2f"), "%"), "Goal Success Rate",
-                    paste0(stepStats[5], " days"), "Longest Goal Streak",
-                    paste0(step30Stats[1]), "30-day Total Steps",
-                    paste0(step30Stats[2]), "30-day Average Daily Steps",
-                    paste0(sprintf(step30Stats[3], fmt = "%#.2f"), "%"), "30-day Goal Success Rate"),
-          x = rep(c(0.605, 0.709), 8), rep(c(seq(0.946, 0.755, length.out = 5),
-                                             seq(0.695, 0.597, length.out = 3)), each = 2), hjust = 0,
-          gp = gpar(cex = 0.5, col = c(rep(c("blue", "black"), 5), rep(c("purple", "black"), 3))))
-grid.segments(x0 = 0.605, y0 = 0.725, x1 = 0.958, y1 = 0.725, gp = gpar(lwd = 0.65))
+# Add labels, key stats by year, and YoY indicators
+yBaseline <- c(0.040, 0.070, 0.100, 0.130)
+for(i in 1:length(pYears)){
+  yAdj <- c(data_pd$stA[i], data_pd$ptA[i], data_pd$maA[i], data_pd$avA[i])
+  grid.segments(x0 = 0.750, y0 = c(0.988, 0.793, 0.597, 0.402, 0.205)[i],
+                x1 = 0.980, y1 = c(0.988, 0.793, 0.597, 0.402, 0.205)[i])
+  grid.segments(x0 = 0.750, y0 = c(0.816, 0.621, 0.425, 0.230, 0.035)[i],
+                x1 = 0.980, y1 = c(0.816, 0.621, 0.425, 0.230, 0.035)[i])
+  grid.points(x = rep(0.970, 4), y = yBaseline + c(0.795, 0.600, 0.403, 0.209, 0.013)[i] + yAdj,
+              pch = c(data_pd$stP[i], data_pd$ptP[i], data_pd$maP[i], data_pd$avP[i]),
+              gp = gpar(col = c(data_pd$stC[i], data_pd$ptC[i], data_pd$maC[i], data_pd$avC[i]),
+                        fill = c(data_pd$stC[i], data_pd$ptC[i], data_pd$maC[i], data_pd$avC[i]),
+                        cex = 0.40))
+  grid.text(label = pYears[i], x = 0.750, y = c(0.961, 0.765, 0.570, 0.375, 0.178)[i],
+            hjust = 0, gp = gpar(cex = 1.00, fontface = "bold"))
+  grid.text(label = c("Longest Goal Streak", "Goal Success Rate",
+                      "Maximum Daily Steps", "Average Daily Steps"),
+            x = rep(0.750, 4), y = yBaseline + c(0.795, 0.600, 0.403, 0.209, 0.013)[i],
+            hjust = 0, gp = gpar(cex = 0.50))
+  if(!anyNA(data_py[, i])){
+    grid.text(label = c(paste(data_py$st[i], "d"), data_py$pt[i], data_py$ma[i], data_py$av[i]),
+              x = rep(0.900, 4), y = yBaseline + c(0.795, 0.600, 0.403, 0.209, 0.013)[i],
+              hjust = 0, gp = gpar(cex = 0.50))}}
 
 # Deactivate grid layout; finalise graphics save
 popViewport()
